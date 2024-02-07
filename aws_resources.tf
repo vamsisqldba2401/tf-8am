1a8b5e3140ca637dcca30e2f47233e3dc103e042
vamsisqldba2401 <147315234+vamsisqldba2401@users.noreply.github.com>
2024-02-07 20:42:48 +0530

    Update aws_resources.tf

--- aws_resources.tf
+++ aws_resources.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "tf-vpc" {
 resource "aws_subnet" "tf-subnet" {
   vpc_id     = aws_vpc.tf-vpc.id
   cidr_block = "10.0.1.0/24"
-availability_zone = "ap-south-1b"
+availability_zone = "ap-south-1a"
 map_public_ip_on_launch = "true"
 
   tags = {
